ead5fcfc81442315884bd09146269a8d339598c6
xiaomo <xiaomo@xiaomo.info>
2020-06-28 20:28:56 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-
-import 'package:flutter_crush/application.dart';
-import 'package:flutter_crush/helpers/audio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_crush/application.dart';
+import 'package:flutter_crush/helpers/audio.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   //
   // Initialize the audio
   //
